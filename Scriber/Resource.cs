6c7546e6a353ea18fe644c14956520cbb5d117f3
Mark Sujew <msujew@yahoo.de>
2020-09-12 16:16:34 +0200

    Added resources and caches

---
+++ Scriber/Resource.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+
+namespace Scriber
+{
+    public class Resource
+    {
+        public Uri Uri { get; }
+
+        private byte[] content;
+
+        public Resource(Uri uri, byte[] content)
+        {
+            Uri = uri ?? throw new ArgumentNullException(nameof(uri));
+            this.content = content ?? throw new ArgumentNullException(nameof(content));
+        }
+
+        public Resource(Uri uri, string content)
+        {
+            if (content is null)
+            {
+                throw new ArgumentNullException(nameof(content));
+            }
+
+            Uri = uri ?? throw new ArgumentNullException(nameof(uri));
+            this.content = Encoding.UTF8.GetBytes(content);
+        }
+
+        public byte[] GetContent()
+        {
+            return content;
+        }
+
+        public string GetContentAsString()
+        {
+            return Encoding.UTF8.GetString(content);
+        }
+
+        public void SetContent(byte[] content)
+        {
+            this.content = content;
+        }
+
+        public void SetContent(string content)
+        {
+            this.content = Encoding.UTF8.GetBytes(content);
+        }
+    }
+}
