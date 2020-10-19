7fb3b431573cb4739eb91122daaf18ccaea31996
Mark Sujew <msujew@yahoo.de>
2020-10-19 20:32:41 +0200

    Fixed alpha page numbering

---
+++ Scriber.Tests/PageNumberingModuleTests.cs
@@ -0,0 +1,57 @@
+﻿using Xunit;
+
+namespace Scriber.Tests
+{
+    public class PageNumberingModuleTests
+    {
+        private PageNumberingModule Module => new PageNumberingModule();
+
+        [Theory]
+        [InlineData(4, PageNumberingStyle.Arabic, "4")]
+        [InlineData(27, PageNumberingStyle.Arabic, "27")]
+        [InlineData(800_000, PageNumberingStyle.Arabic, "800000")]
+        [InlineData(4, PageNumberingStyle.Alphabet, "d")]
+        [InlineData(27, PageNumberingStyle.Alphabet, "aa")]
+        [InlineData(800_000, PageNumberingStyle.Alphabet, "asmkf")]
+        [InlineData(27, PageNumberingStyle.AlphabetUpper, "AA")]
+        [InlineData(4, PageNumberingStyle.Roman, "iv")]
+        [InlineData(4, PageNumberingStyle.RomanUpper, "IV")]
+        public void ValueTest(int value, PageNumberingStyle style, string result)
+        {
+            var module = Module;
+            module.Style = style;
+            module.Current = value;
+            var x = module.Next();
+            Assert.Equal(result, x);
+        }
+
+        [Fact]
+        public void NextTest()
+        {
+            var module = Module;
+            module.Current = 4;
+            module.Style = PageNumberingStyle.Arabic;
+            Assert.Equal("4", module.Next());
+            Assert.Equal("5", module.Next());
+        }
+
+        [Fact]
+        public void LessThanOne()
+        {
+            var module = Module;
+            module.Current = -50;
+            Assert.Equal(1, module.Current);
+        }
+
+        [Fact]
+        public void ResetSuccess()
+        {
+            var module = Module;
+            module.Current = 15;
+            module.Style = PageNumberingStyle.Roman;
+            module.Reset();
+            Assert.Equal(1, module.Current);
+            Assert.Equal(PageNumberingStyle.Arabic, module.Style);
+        }
+    }
+}
--- Scriber/PageNumberingModule.cs
+++ Scriber/PageNumberingModule.cs
@@ -1,4 +1,5 @@
 ﻿using Scriber.Localization;
+using System;
 
 namespace Scriber
 {
@@ -40,12 +41,41 @@ namespace Scriber
             return Style switch
             {
                 PageNumberingStyle.Arabic => value.ToString(),
-                PageNumberingStyle.Alphabet => ((char)(value + 'a' - 1)).ToString(),
-                PageNumberingStyle.AlphabetUpper => ((char)(value + 'A' - 1)).ToString(),
+                PageNumberingStyle.Alphabet => AlphaBase(value),
+                PageNumberingStyle.AlphabetUpper => AlphaBase(value).ToUpperInvariant(),
                 PageNumberingStyle.Roman => Locale.FormatRomanNumber(value),
                 PageNumberingStyle.RomanUpper => Locale.FormatRomanNumber(value).ToUpperInvariant(),
                 _ => value.ToString(),
             };
+        }
+
+        private static readonly long[] Base26 = new long[7];
+
+        static PageNumberingModule()
+        {
+            Base26[0] = 1;
+            for (int i = 1; i < Base26.Length; i++)
+            {
+                Base26[i] = Base26[i - 1] * 26;
+            }
+        }
+
+        private static string AlphaBase(long value)
+        {
+            if (value > Base26[6] || value < 1)
+            {
+                return string.Empty;
+            }
+
+            int index = 0;
+            while (index < 6 && Base26[index + 1] < value)
+            {
+                index++;
+            }
+            var item = Base26[index];
+            var times = value / item;
+            var result = times * item;
+            return ((char)('a' + times - 1)).ToString() + AlphaBase(value - result);
         }
     }
 }
